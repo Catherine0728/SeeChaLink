a953e4afd2f2348fbbd42fdfd85b1695f5bf4ac4
Catherine0728 <15520054289@163.com>
2015-06-17 10:38:14 +0800

    SeeChalink_five
    there is a library has added in.

---
+++ SeeChaLink/src/com/home/mainactivity/SetTimingActivity.java
@@ -0,0 +1,18 @@
+package com.home.mainactivity;
+
+import android.app.Activity;
+
+/**
+ * @author Catherine 这是设置定时启动的页面
+ * 
+ * */
+public class SetTimingActivity extends Activity {
+	public static String TAG = "SetTiming";
+
+	@Override
+	public void setContentView(int layoutResID) {
+		// TODO Auto-generated method stub
+		super.setContentView(layoutResID);
+	}
+
+}
